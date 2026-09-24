a4c6a8ab86ef0b36e6d78cfa2da65e064c4d9715
DenGame <dengameua@gmail.com>
2026-09-24 04:20:20 +0300

    Update Unit1.pas

--- Unit1.pas
+++ Unit1.pas
@@ -51,12 +51,9 @@ type
       Shift: TShiftState; X, Y: Integer);
     procedure Speichern1Click(Sender: TObject);
     procedure ffnen1Click(Sender: TObject);
-//    procedure MitPillenauffllen1Click(Sender: TObject);
-//    procedure MitWndenauffllen1Click(Sender: TObject);
-//    procedure MitSuperpillenauffllen1Click(Sender: TObject);
     procedure Beenden1Click(Sender: TObject);
     procedure Neu1Click(Sender: TObject);
-//    procedure Save1Click(Sender: TObject);
+
   private
     { Private-Deklarationen }
 //		procedure WMDropFiles (var Msg: TMessage); message wm_DropFiles;
@@ -77,48 +74,13 @@ implementation
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-
-
-
-//bmp := TBitmap.Create;
-//  bmp.Width := 2600;
-//  bmp.Height := 2600;
-
-//	DragAcceptFiles(Handle, True);
-//  form1.VertScrollBar.Tracking:=true;
-//  form1.HorzScrollBar.Tracking:=true;
-//  clientwidth:=800;
-//  clientheight:=800;
   neu1click(sender);
 end;
-
-//procedure TForm1.WMDropFiles(var Msg: TMessage);
-
-//var
-//	NameLength : integer;
-//	Fname : String;
-//	TempFile: array[0..255] of Char;
-
-//begin
-
-//	try
-//		screen.cursor := crHourglass;
-//		NameLength := DragQueryFile(Msg.wParam, 0, Nil , 0);
-//		DragQueryFile(Msg.wParam, 0, TempFile, NameLength+1);
-//		fname := StrPas(TempFile);
-//	finally
-//		screen.cursor := crDefault;
-//	end;
-//end;
 
 procedure TForm1.FormPaint(Sender: TObject);
 
 var
   x,y:integer;
-//    K, Bl, CC: Byte;
-//      MT, L, Long: LongInt;
-
-
 begin
 
 if RadioButton1.Checked = True then
@@ -162,9 +124,6 @@ if RadioButton1.Checked = True then
     thetool:=0
    end;
 
-
-//    L:=L+(Bl Shl (8*K));
-
   for x:=0 to 31 do
     for y:=0 to 31 do
       case map[x,y] of
@@ -174,61 +133,33 @@ if RadioButton1.Checked = True then
         end;
               
       256:begin
-  Canvas.Draw(x*20,y*20,image1.picture.bitmap);
+          Canvas.Draw(x*20,y*20,image1.picture.bitmap);
         end;
       512:begin   //114
-//          canvas.brush.color:=$ff77ff;
           canvas.draw(x*20,y*20,image2.picture.bitmap);
-//          Image7.Canvas.Draw(x*40,y*40,image2.picture.bitmap);
-//          PaintBox1.Canvas.Draw(x*40,y*40,image2.picture.bitmap);
         end;
       768:begin   //87
-//          canvas.brush.color:=$ff;
-//          canvas.rectangle(x*16,y*16,x*16+17,y*16+17);
           canvas.draw(x*20,y*20,image3.picture.bitmap);
-//          PaintBox1.Canvas.Draw(x*40,y*40,image3.picture.bitmap);
         end;
       1024:begin
-//          canvas.brush.color:=$ff00;
-//          canvas.rectangle(x*16,y*16,x*16+17,y*16+17);
           canvas.draw(x*20,y*20,image4.picture.bitmap);
-//          PaintBox1.Canvas.Draw(x*40,y*40,image4.picture.bitmap);
         end;
       1280:begin
-//          canvas.brush.color:=$ff0000;
           canvas.draw(x*20,y*20,image5.picture.bitmap);
-//          PaintBox1.Canvas.Draw(x*40,y*40,image5.picture.bitmap);
         end;
       1536:begin
-//          canvas.brush.color:=$77ff;
-//          canvas.rectangle(x*16,y*16,x*16+17,y*16+17);
           canvas.draw(x*20,y*20,image6.picture.bitmap);
-//          PaintBox1.Canvas.Draw(x*40,y*40,image6.picture.bitmap);
         end;
       1792:begin   //114
-//          canvas.brush.color:=$ff77ff;
           canvas.draw(x*20,y*20,image7.picture.bitmap);
-//          Image7.Canvas.Draw(x*40,y*40,image2.picture.bitmap);
-//          PaintBox1.Canvas.Draw(x*40,y*40,image2.picture.bitmap);
         end;
       2048:begin   //114
-//          canvas.brush.color:=$ff77ff;
           canvas.draw(x*20,y*20,image8.picture.bitmap);
-//          Image7.Canvas.Draw(x*40,y*40,image2.picture.bitmap);
-//          PaintBox1.Canvas.Draw(x*40,y*40,image2.picture.bitmap);
         end;
       2304:begin   //114
-//          canvas.brush.color:=$ff77ff;
           canvas.draw(x*20,y*20,image9.picture.bitmap);
-//          Image7.Canvas.Draw(x*40,y*40,image2.picture.bitmap);
-//          PaintBox1.Canvas.Draw(x*40,y*40,image2.picture.bitmap);
         end;
-
-
       end;
-
-//bmp := PaintBox1.Canvas;
-
 end;
 
 procedure TForm1.FormMouseDown(Sender: TObject; Button: TMouseButton;
@@ -240,13 +171,9 @@ begin
 end;
 
 procedure TForm1.FormMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
-
 var
   a,b:integer;
-
 begin
-//  if (y>40) and (y<1600) then begin
-
     if mdown=1 then begin
       if thetool=9 then
         for a:=0 to 31 do
@@ -259,7 +186,6 @@ begin
       map[x div 20,y div 20]:=0;
       Formpaint(sender);
     end;
-//  end;
 end;
 
 procedure TForm1.FormMouseUp(Sender: TObject; Button: TMouseButton;
@@ -269,11 +195,9 @@ begin
 end;
 
 procedure TForm1.Speichern1Click(Sender: TObject);
-
 var
   x,y:integer;
   f:file;
-
 begin
   if savedialog1.execute then begin
     assignfile(f, savedialog1.filename);
@@ -287,11 +211,9 @@ begin
 end;
 
 procedure TForm1.ffnen1Click(Sender: TObject);
-
 var
   x,y:integer;
   f:file;
-
 begin
   if opendialog1.execute then begin
     assignfile(f,opendialog1.filename);
@@ -304,65 +226,19 @@ begin
   end;
 end;
 
-//procedure TForm1.MitPillenauffllen1Click(Sender: TObject);
-
-//var
-//  x,y:integer;
-
-//begin
-//  for x:=0 to 25 do
-//    for y:=0 to 17 do
-//      if map[x,y]=0 then map[x,y]:=2;
-//  invalidate;
-//end;
-
-//procedure TForm1.MitWndenauffllen1Click(Sender: TObject);
-
-//var
-//  x,y:integer;
-
-//begin
-//  for x:=0 to 25 do
-//    for y:=0 to 17 do
-//      if map[x,y]=0 then map[x,y]:=1;
-//  invalidate;
-//end;
-
-//procedure TForm1.MitSuperpillenauffllen1Click(Sender: TObject);
-
-//var
-//  x,y:integer;
-
-//begin
-//  for x:=0 to 25 do
-//    for y:=0 to 17 do
-//      if map[x,y]=0 then map[x,y]:=3;
-//  invalidate;
-//end;
-
 procedure TForm1.Beenden1Click(Sender: TObject);
 begin
   close;
 end;
 
 procedure TForm1.Neu1Click(Sender: TObject);
-
 var
   x,y:integer;
-
 begin
   for x:=0 to 31 do begin
     for y:=0 to 31 do
       map[x,y]:=0;
-//    map[x,0]:=114;
-//    map[x,0]:=117;
   end;
   invalidate;
 end;
-
-//procedure TForm1.Save1Click(Sender: TObject);
-//begin
-// bmp.SaveToFile ( 'nuf.bmp' );
-//end;
-
 end.
